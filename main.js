0c12ab759570a3a646d0f96edd42092e0c6e666e
Asia Keomanivong <asia.keomanivong@gmail.com>
2021-05-13 23:32:29 -0600

    another attempt

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const express= require("express"), app=express(),
     app = express(),
-    router = express.Router(),
+    router = require('./routes/index'),
     homeController = require("./controllers/homeController"),
     errorController = require("./controllers/errorController"),
     usersController = require("./controllers/usersController"),
@@ -24,91 +24,55 @@ mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: tr
 app.set("port", process.env.PORT ||3000);
 app.set("view engine", "ejs");
 
-router.use(express.static("public"));
-router.use(layouts);
-router.use(expressValidator());
-router.use(
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(layouts);
+app.use(expressValidator());
+app.use(
     express.urlencoded({
         extended: false
     })
 );
 
-router.use(methodOverride("_method", { methods: ['POST', 'GET'] }));
+app.use(methodOverride("_method", {methods: ['POST', 'GET']}));
 
-router.use(express.json());
+app.use(express.json());
 
-router.use(cookieParser("my_passcode"));
-router.use(expressSession({
+app.use(cookieParser("my_passcode"));
+app.use(expressSession({
     secret: "my_passcode",
     cookie: {
-        maxAge: 400000
+        maxAge: 360000
     },
     resave: false,
     saveUninitialized: false
 }));
 
 
-router.use(passport.initialize());
-router.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-router.use(connectFlash());
+app.use(connectFlash());
 
-router.use((req, res, next) => {
+
+
+
+
+
+app.use((req, res, next) => {
     res.locals.flashMessages = req.flash();
     res.locals.loggedIn = req.isAuthenticated();
     res.locals.currentUser = req.user;
     next();
 })
 
-router.get("/", homeController.index);
-
-
-router.get("/", homeController.index, homeController.showIndex);
-
-
-//Posts
-router.post("/post", postsController.create, postsController.redirectView, postsController.index);
-router.get("/posts", postsController.index, postsController.indexView)
-router.get("/posts/new", postsController.new);
-router.get("/posts/:id", postsController.show, postsController.showView);
-router.get("/posts/:id/edit", postsController.edit);
-router.put("/post/:id/update", postsController.update, postsController.redirectView);
-router.delete("/posts/:id/delete", postsController.delete, postsController.redirectView);
-
-//Users
-router.get("/users", usersController.index, usersController.indexView);
-router.get("/users/new", usersController.new);
-router.post("/user", usersController.create, usersController.redirectView);
-
-router.get("/users/login", usersController.login);
-router.post("/users/login", usersController.authenticate);
-router.get("/users/logout", usersController.logout, usersController.redirectView);
-
-
-router.get("/users/:id", usersController.show, usersController.showView);
-router.get("/users/:id/edit", usersController.edit);
-router.put("/users/:id/update", usersController.update, usersController.redirectView);
-router.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
-
-router.get("/api/users", usersController.index, usersController.respondJSON);
-router.get("/users/:id/join", usersController.join, usersController.respondJSON);
-router.use(usersController.errorJSON);
-
-//router.get("/users", usersController.index,usersController.indexView);
-router.get("/login", homeController.getLogin);
-router.get("/signup", usersController.getUsersPage);
-// router.post("/user",usersController.saveUser);
-
-
-
-router.use(errorController.pageNotFoundError);
-router.use(errorController.interalServerError);
-
 app.use("/", router);
 
 const server = app.listen(app.get("port"), () => {
-    console.log(`Server is running on port at http://localhost:${app.get("port")}`)
+    console.log(`Server is running on port:${app.get("port")}`)
 
 });
